664d60c833af44cab9cd41155fbd3e45ea6bca5a
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-10-28 23:56:10 -0300

    configure los middlewares

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 
 const app = express();
 app.set("port", process.env.PORT || 4000);
 
-app.listen(app.get("port"),()=>{
-    console.log('Estoy en el puerto '+app.get("port"));
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto " + app.get("port"));
 });
 
-console.log("hola mundoooooo");
+// middlewares son funciones que se ejecutan antes de las rutas.
+app.use(morgan("dev"));
+// permitir peticiones remotas
+app.use(cors());
+// middlewares para interpretar peticiones remotas
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// cargar un archivo estatico
+app.use(express.static(path.join(__dirname, "../public")));
+console.log(path.join(__dirname, "../public"));
+
+// rutas :nombre de dominio +-----
+app.get("/prueba", (req, res) => {
+  res.send("Esto es una prueba de la peticion GET");
+});
