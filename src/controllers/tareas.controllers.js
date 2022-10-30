72080ffe7fb82e4ad989ce6312fb47a5ed392f05
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-10-30 18:00:48 -0300

    agregue la logica para crear y obtener los productos

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,13 +1,35 @@
-
- export const listarTareas=(req, res) => {
-    res.send("Aqui tengo que retornar una lista de tareas");
+import Tarea from "../models/tarea";
+export const listarTareas = async (req, res) => {
+  try {
+    // guardar ese producto en la bd
+    const listaTareas = await Tarea.find();
+    // rersponder al usuario que todo salio bien
+    res.status(200).json(listaTareas);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al intentar buscar las tareas",
+    });
   }
+};
 
-
- export  const crearTarea = (req, res) => {
+export const crearTarea = async (req, res) => {
   // extraer del body los datos
-console.log(req.body)
-// agregar la validacion correspondiente 
-// guardar ese producto en la bd 
-    res.send("Esto es una prueba de la peticion post");
+  console.log(req.body);
+  // agregar la validacion correspondiente
+  try {
+    console.log(req.body);
+    const tareaNueva = new Tarea(req.body);
+    // guardar ese producto en la bd
+    await tareaNueva.save();
+    // rersponder al usuario que todo salio bien
+    res.status(201).json({
+      mensaje: "El producto fue correctamente creado",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "Error al intentar agregar un producto",
+    });
   }
+};
