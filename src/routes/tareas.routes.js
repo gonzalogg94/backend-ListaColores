11ba1113b2bd9934dd3dce5ec766f4b9fc3515df
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-11-05 20:38:48 -0300

    uni el backend con el front

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -51,12 +51,4 @@ router
 
 export default router;
 
-// app.get("/tareas", (req, res) => {
-//     res.send("Aqui tengo que retornar una lista de tareas");
-//   });
-//   app.post("/tareas", (req, res) => {
-//     res.send("retornar un objeto");
-//   });
-//   app.get("/tareas2", (req, res) => {
-//     res.send("Aqui devolvemos una tarea");
-//   });
+
